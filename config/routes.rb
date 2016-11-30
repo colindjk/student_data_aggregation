209be5fdb97ae5bf3bd696cb20eec6131dd23e57
Colin Kirkpatrick <kibbles@kibblesnbits.server.world>
2016-11-30 06:20:48 -0500

    Added by_day and by_user routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   root "graphs#index"
   get "/by_day" => 'graphs#by_day'
+  get "/by_user" => 'graphs#by_user'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
