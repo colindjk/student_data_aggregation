aae97c12b917063ed9e99b563fa8a5f3444e4000
Colin Kirkpatrick <kibbles@kibblesnbits.server.world>
2016-11-29 22:10:13 -0500

    Introduced dependencies on odsa_* tables

--- app/models/by_day.rb
+++ app/models/by_day.rb
@@ -1,5 +1,10 @@
+# Representation for 'by_day' table.
 class ByDay < ActiveRecord::Base
-  #has_many :odsa_user_interactions
+
+  # This is simply demonstrating the tables which 'by_day' will
+  # be using as resources. The relationships are subject to change
+  # while the 'by_day' table remains under construction.
+  has_many :odsa_user_interactions
   has_many :odsa_exercise_attempts
   has_many :odsa_exercise_progresses
   has_many :odsa_module_progresses
--- app/models/by_user.rb
+++ app/models/by_user.rb
@@ -1,2 +1,8 @@
 class ByUser < ActiveRecord::Base
+  has_and_belong_to_many :users
+  has_many :odsa_user_interactions
+  has_many :odsa_exercise_attempts
+  has_many :odsa_exercise_progresses
+  has_many :odsa_module_progresses
+  has_many :odsa_book_progresses
 end
