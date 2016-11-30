f7c216bc2b074c21a2fe70ca0d6078516959cfad
Colin Kirkpatrick <kibbles@kibblesnbits.server.world>
2016-11-29 22:09:37 -0500

    Updated migration files to include pre-existing odsa_* tables

--- db/migrate/20161130013614_create_by_days.rb
+++ db/migrate/20161130013614_create_by_days.rb
@@ -1,8 +1,17 @@
 class CreateByDays < ActiveRecord::Migration
   def change
     create_table :by_days do |t|
+      t
 
-      t.timestamps null: false
+      # TODO: Keep this updated with 
+      # app/model/by_day.rb
+      has_many :odsa_user_interactions
+      has_many :odsa_exercise_attempts
+      has_many :odsa_exercise_progresses
+      has_many :odsa_module_progresses
+      has_many :odsa_book_progresses
+
+      #t.timestamps null: false
     end
   end
 end
--- db/migrate/20161130025014_create_by_users.rb
+++ db/migrate/20161130025014_create_by_users.rb
@@ -1,8 +1,19 @@
 class CreateByUsers < ActiveRecord::Migration
   def change
     create_table :by_users do |t|
+      t.string :user, null: false
 
-      t.timestamps null: false
+      # TODO: Keep this updated with 
+      # app/model/by_user.rb
+      has_and_belong_to_many :users
+      has_many :odsa_user_interactions
+      has_many :odsa_exercise_attempts
+      has_many :odsa_exercise_progresses
+      has_many :odsa_module_progresses
+      has_many :odsa_book_progresses
+      # t.timestamps null: false
+      # As this table is recording information by_user over a long
+      # period of time (aggregation), it will not have timestamps.
     end
   end
 end
