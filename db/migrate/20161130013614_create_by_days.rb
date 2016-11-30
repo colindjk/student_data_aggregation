4cb708676857d332c69b49704bc4bdd7d94429d3
Colin Kirkpatrick <kibbles@kibblesnbits.server.world>
2016-11-30 05:37:19 -0500

    Updated and moving to seeding via seeds.rb

--- db/migrate/20161130013614_create_by_days.rb
+++ db/migrate/20161130013614_create_by_days.rb
@@ -1,24 +1,18 @@
 class CreateByDays < ActiveRecord::Migration
 
-  def change
+  def up
     create_table :by_days do |t|
-      t.string :day_of, null: false
-      t.integer :attempts, null: false
-      t.integer :correct, null: false
-      t.integer :users, null: false
-
-      # TODO: Keep this updated with 
-      # app/model/by_day.rb
-      #has_many :odsa_user_interactions
-      #has_many :odsa_exercise_attempts
-      #has_many :odsa_exercise_progresses
-      #has_many :odsa_module_progresses
-      #has_many :odsa_book_progresses
-
-      #t.timestamps null: false
+      t.string  :day_of,    null: false
+      t.integer :attempts,  null: false
+      t.integer :correct,   null: false
+      t.integer :users,     null: false
     end
+
   end
 
+  def down
+    drop_table :by_days
+  end
 
 
 end
--- db/migrate/20161130025014_create_by_users.rb
+++ db/migrate/20161130025014_create_by_users.rb
@@ -1,30 +1,21 @@
 class CreateByUsers < ActiveRecord::Migration
 
   #! TODO: Move code to aggregate to up
-  #! This could contain the code to actually aggregate the data.
+  #! This could contain the code to actually
+  #! aggregate the data.
   def up
-
-  end
-
-  def change
     create_table :by_users do |t|
       # Fields section
       t.string :user, null: false
       # Operating system.
       t.string :os
-
-      # TODO: Keep this updated with 
-      # app/model/by_user.rb
-      #has_and_belong_to_many :users
-      #has_many :odsa_user_interactions
-      #has_many :odsa_exercise_attempts
-      #has_many :odsa_exercise_progresses
-      #has_many :odsa_module_progresses
-      #has_many :odsa_book_progresses
-
-      # t.timestamps null: false
-      # As this table is recording information by_user over a long
-      # period of time (aggregation), it will not have timestamps.
     end
   end
+
+
+
+  def down
+    drop_table :by_users
+  end
+
 end
